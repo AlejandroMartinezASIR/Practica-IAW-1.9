6e631e1cbfa71229c503729999d138bc7cce779a
John Doe <johndoe@example.com>
2024-11-25 12:11:57 +0000

    seg

---
+++ script/deploy_wordpress_backend.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Muestra todos los comandos que se van ejecutadno
+set -x
+
+# Paso1. Se importan las variables de configuracion
+source .env
+
+systemctl restart mysql
+
+# Creamos la base de datos y el usuario de la base de datos
+mysql -u root <<< "DROP DATABASE IF EXISTS $WORDPRESS_DB_NAME"
+mysql -u root <<< "CREATE DATABASE $WORDPRESS_DB_NAME"
+mysql -u root <<< "DROP USER IF EXISTS $WORDPRESS_DB_USER@$IP_CLIENTE_MYSQL"
+mysql -u root <<< "CREATE USER $WORDPRESS_DB_USER@$IP_CLIENTE_MYSQL IDENTIFIED BY '$WORDPRESS_DB_PASSWORD'"
+mysql -u root <<< "GRANT ALL PRIVILEGES ON
