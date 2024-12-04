7d06d8a01dea098aa4220eb5e32f8efc9fffaf13
AlejandroMartinezASIR <alejandromartinezasir@gmail.com>
2024-12-04 18:14:56 +0100

    MIAU

--- script/deploy_wordpress_frontend.sh
+++ script/deploy_wordpress_frontend.sh
@@ -1,61 +1,50 @@
 #!/bin/bash
 
-#Muestra todos los comandos que se van ejecutadno
 set -ex
 
-# Actualizamos los repositorios
 apt update
 
-# Actualizamos los paquetes 
-apt upgrade -y
+#apt upgrade -y
 
-# Ponemos las variables del archivo .env
 source .env
 
-# Borramos instalaciones previas de wp-cli
 rm -rf /tmp/wp-cli.phar
 
-# Descargamos el archivo wp-cli.phar del repositorio oficial de WP-CLI. 
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
 
-# Le asignamos permisos de ejecución al archivo wp-cli.phar.
 chmod +x wp-cli.phar
 
-# Movemos el archivo wp-cli.phar al directorio /usr/local/bin/ con el nombre wp para poder utilizarlo sin necesidad de escribir la ruta completa donde se encuentra.
-mv wp-cli.phar /usr/local/bin/wp
+mv /tmp/wp-cli.phar /usr/local/bin/wp
 
-# Eliminamos instalaciones revias de WordPress
 rm -rf /var/www/html/*
 
-# Descargamos el código fuente de WordPress en /var/www/html
 wp core download --locale=es_ES --path=/var/www/html --allow-root
 
-# Creamos el archivo wp-config
 wp config create \
   --dbname=$WORDPRESS_DB_NAME \
   --dbuser=$WORDPRESS_DB_USER \
   --dbpass=$WORDPRESS_DB_PASSWORD \
   --dbhost=$WORDPRESS_DB_HOST \
-  --path=$WORDPRESS_DIRECTORY \
+  --path=/var/www/html \
   --allow-root
 
 wp core install \
-  --url=practica-wordpress.ddns.net \
-  --title="$WORDPRESS_TITLE" \
-  --admin_user=$WORDPRESS_USER \
-  --admin_password=$WORDPRESS_PASSWORD \
-  --admin_email=$WORDPRESS_EMAIL \
-  --path=$WORDPRESS_DIRECTORY \
-  --allow-root  
+  --url=$CERTIFICATE_DOMAIN \
+  --title="$WORDPRESS_TITTLE" \
+  --admin_user=$WORDPRESS_ADMIN_USER \
+  --admin_password=$WORDPRESS_ADMIN_PASS \
+  --admin_email=$WORDPRESS_ADMIN_EMAIL \
+  --path=/var/www/html \
+  --allow-root
 
-cp ../htaccess/.htaccess $WORDPRESS_DIRECTORY --allow-root
+cp ../htaccess/.htaccess /var/www/html/
 
-wp plugin install wps-hide-login --activate --path=$WORDPRESS_DIRECTORY --allow-root
+rm -rf /var/www/html/wp-config.php
 
-wp option update whl_page "$WORDPRESS_HIDE_LOGIN_URL" --path=$WORDPRESS_DIRECTORY --allow-root
+wp plugin install wps-hide-login --activate --path=/var/www/html --allow-root
 
-wp rewrite structure '/%postname%/' --path=$WORDPRESS_DIRECTORY --allow-root
+wp rewrite structure '/%postname%/' --path=/var/www/html --allow-root
 
-wp rewrite flush --allow-root
+wp rewrite flush
 
-sudo chown -R www-data:www-data $WORDPRESS_DIRECTORY --allow-root
+chown -R www-data:www-data /var/www/html/
--- script/install_lamp_back.sh
+++ script/install_lamp_back.sh
@@ -11,17 +11,17 @@ apt update
 #Actualizar los paquetes del sistema
 apt upgrade -y
 
-#Instalamos el servidor web apache y poniendo -y no hace falta escribir nada
-apt install apache2 -y
-
 #Instalamos MySQL server
 apt install mysql-server -y
 
 #configuramos el config de mysql
 sed -i s/127.0.0.1/$BACKEND_MYSQL_PRIVATE_IP/ /etc/mysql/mysql.conf.d/mysqld.cnf
 
+sudo mysql -u root <<< "DROP USER IF EXISTS '$WORDPRESS_DB_USER'@'$IP_CLIENTE_MYSQL';"
+sudo mysql -u root <<< "CREATE USER '$WORDPRESS_DB_USER'@'$IP_CLIENTE_MYSQL' IDENTIFIED BY '$WORDPRESS_DB_PASSWORD';"
+sudo mysql -u root <<< "GRANT ALL PRIVILEGES ON \`$WORDPRESS_DB_NAME\`.* TO '$WORDPRESS_DB_USER'@'$IP_CLIENTE_MYSQL';"
+sudo mysql -u root <<< "FLUSH PRIVILEGES;"
+
 #reiniciamos el servicio
 systemctl restart mysql
-
-systemctl daemon-reload
 
